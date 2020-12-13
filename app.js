45efb6fcb3c66120f5d9d8179247951362403c20
Jose Sanchez-Capo <josesanchezcapo@me.com>
2020-12-12 19:35:20 -0600

    Adding Inquiries for Employee Profile

--- app.js
+++ app.js
@@ -5,14 +5,63 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
+// This Object will hold User Input
+let employeeProfile = [];
+
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const { type } = require("os");
+const { choices } = require("yargs");
 
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+
+// Employee Profile Questions
+
+const employeeProfileQuestions = function () {
+    inquirer.prompt([
+        {
+
+            type: 'input',
+            name: 'name',
+            message: 'Enter your name'
+
+        },
+        {
+
+            type: 'input',
+            name: 'id',
+            message: 'Enter Team Member Id number'
+
+
+        },
+        {
+
+            type: 'input',
+            name: 'email',
+            message: 'Enter Team Member email address'
+
+        },
+        {
+
+            type: 'list',
+            name: 'profile',
+            message: 'Select the role for the Team Member Profile to be created',
+            choices: ['Engineer', 'Intern', 'Manager']
+
+        }
+    ]).then(responses => {
+        // HINT: each employee type (manager, engineer, or intern) has slightly different
+        // information; write your code to ask different questions via inquirer depending on
+        // employee type.
+        // Profile Specifics
+
+
+    })
+}
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
@@ -24,9 +73,8 @@ const render = require("./lib/htmlRenderer");
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+employeeProfileQuestions();
+
 
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
 // and Intern classes should all extend from a class named Employee; see the directions
